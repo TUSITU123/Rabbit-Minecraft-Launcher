76faa99b0f264f39d2651c1e365f4d04772cc265
TUSITU123 <79701114+TUSITU123@users.noreply.github.com>
2022-04-16 14:09:57 +0800

    上传代码

---
+++ Form1.cs
@@ -0,0 +1,18 @@
+using Sunny.UI;
+using ProjBobcat;
+
+namespace Rabbit_Minecraft_Launcher
+{
+    public partial class Form1 : UIForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void uiButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
